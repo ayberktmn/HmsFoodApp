bb027bc4ffa17e5e8d4703dd5d4a832b9a7260d8
ayberktemin <ayberktemin@gmail.com>
2023-10-11 18:25:50 +0300

    profile screen fixed

--- app/src/main/java/com/ayberk/foodapp/MainActivity.kt
+++ app/src/main/java/com/ayberk/foodapp/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.ayberk.foodapp
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import androidx.navigation.fragment.findNavController
 import android.os.Looper
+import android.text.TextUtils
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.ayberk.foodapp.databinding.ActivityMainBinding
 import com.google.android.filament.View
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.common.ApiException
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,6 +27,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        getToken()
     }
+
+    private fun getToken() {
+        // Create a thread.
+        object : Thread() {
+            override fun run() {
+                try {
+                    // Obtain the app ID from the agconnect-services.json file.
+                    val appId = "109290863"
+
+                    // Set tokenScope to HCM.
+                    val tokenScope = "HCM"
+                    val token = HmsInstanceId.getInstance(this@MainActivity).getToken(appId, tokenScope)
+                    Log.i(ContentValues.TAG, "My get token:$token")
+//                    Toast.makeText(this@MainActivity,"Token:",Toast.LENGTH_SHORT).show()
+
+                    // Check whether the token is null.
+                    if (!TextUtils.isEmpty(token)) {
+                        sendRegTokenToServer(token)
+                    }
+                } catch (e: ApiException) {
+                    Log.e(ContentValues.TAG, "get token failed, $e")
+                }
+            }
+        }.start()
+    }
+    private fun sendRegTokenToServer(token: String) {
+        Log.i(ContentValues.TAG, "sending token to server. token:$token")
+    }
+
 }
--- app/src/main/java/com/ayberk/foodapp/ProfileFragment.kt
+++ app/src/main/java/com/ayberk/foodapp/ProfileFragment.kt
@@ -1,9 +1,11 @@
 package com.ayberk.foodapp
 
+import android.content.IntentSender
 import android.graphics.Color
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,17 +19,23 @@ import com.ayberk.foodapp.databinding.FragmentProfileBinding
 import com.bumptech.glide.Glide
 import com.huawei.hmf.tasks.Task
 import com.huawei.hms.common.ApiException
+import com.huawei.hms.iap.Iap
+import com.huawei.hms.iap.IapApiException
+import com.huawei.hms.iap.IapClient
+import com.huawei.hms.iap.entity.OrderStatusCode
+import com.huawei.hms.iap.entity.ProductInfo
+import com.huawei.hms.iap.entity.ProductInfoReq
 import com.huawei.hms.support.account.AccountAuthManager
 import com.huawei.hms.support.account.request.AccountAuthParams
 import com.huawei.hms.support.account.request.AccountAuthParamsHelper
 import com.huawei.hms.support.account.result.AuthAccount
 import com.huawei.hms.support.account.service.AccountAuthService
+import com.huawei.hms.support.api.client.Status
 
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,12 +56,18 @@ class ProfileFragment : Fragment() {
         task.addOnSuccessListener { authAccount ->
             val username = authAccount.displayName
             val photo = authAccount.avatarUri
+            val gender = authAccount.gender
+            val email = authAccount.email
 
             Glide.with(binding.imgUser)
                 .load(photo)
                 .circleCrop()
                 .into(binding.imgUser)
             binding.txtUserName.text = username
+          //  binding.txtEmail.text = email
+
+            println("gender:${gender}")
+            println("gelen email:${email}")
 
         }
         task.addOnFailureListener { e -> // The silent sign-in fails. Your app will call getSignInIntent() to show the authorization or sign-in screen.
@@ -67,12 +81,14 @@ class ProfileFragment : Fragment() {
                 //  startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN)
             }
         }
-
         binding.btnSignout.setOnClickListener {
             signOut()
         }
         binding.btnCancel.setOnClickListener {
             showCancelAuthorizationConfirmationDialog()
+        }
+        binding.btnPremium.setOnClickListener {
+            loadProduct()
         }
         return view
     }
@@ -130,4 +146,46 @@ class ProfileFragment : Fragment() {
           val alertDialog = alertDialogBuilder.create()
           alertDialog.show()
       }
+
+    private fun loadProduct() {
+        // Obtain in-app product details configured in AppGallery Connect, and then show the products.
+        val iapClient = Iap.getIapClient(requireContext())
+        val task =iapClient.obtainProductInfo(createProductInfoReq())
+        task.addOnSuccessListener { result ->
+            if (result != null && !result.productInfoList.isEmpty()) {
+
+            }
+        }.addOnFailureListener { e ->
+            e.message?.let { Log.e("IAP", it) }
+            if (e is IapApiException) {
+                val returnCode = e.statusCode
+                if (returnCode == OrderStatusCode.ORDER_HWID_NOT_LOGIN) {
+                    Toast.makeText(requireContext(), "Please sign in to the app with a HUAWEI ID.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(requireContext(), "error", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun createProductInfoReq(): ProductInfoReq? {
+        // In-app product type contains:
+        // 0: consumable.
+        // 1: non-consumable.
+        // 2: auto-renewable subscription.
+        val req = ProductInfoReq()
+        req?.let { productDetails ->
+            productDetails.priceType = IapClient.PriceType.IN_APP_CONSUMABLE
+
+            val productIds = ArrayList<String>()
+            // Pass in the item_productId list of products to be queried.
+            // The product ID is the same as that you set during product information configuration in AppGallery Connect.
+            productIds.add("Consumable_1")
+            productDetails.productIds = productIds
+        }
+        return req
+    }
 }
+
